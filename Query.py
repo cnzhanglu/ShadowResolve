8613ced49841ea4cd09ce1f939edd461378c25f6
zhanglu <zhanglu@chihuo.fun>
2024-03-03 18:38:53 +0800

    new

---
+++ Query.py
@@ -0,0 +1,172 @@
+import argparse
+import csv
+import io
+import ipaddress
+import os
+import socket
+import sys
+from concurrent.futures import ThreadPoolExecutor
+
+from tqdm import tqdm
+
+from Resolve import DNSHandler, setup_logger
+
+
+def parse_arguments():
+    """
+    解析命令行参数
+    """
+    parser = argparse.ArgumentParser(
+        description='ShadowResolve 数据包发送工具\n支持伪造源IP，也支持使用 ecs_client 地址的方式')
+
+    # 添加命令行参数
+    parser.add_argument('-s', '--spoof-source', action='store_true',
+                        help='是否伪造源地址。默认为否。如果提供文件路径作为参数，则视为启用。')
+    parser.add_argument('-e', '--ecs-enable', action='store_true',
+                        help='是否使用 ECS 携带源地址。默认为否。如果提供文件路径作为参数，则视为启用。')
+    parser.add_argument('-d', '--dnsserver', type=validate_ip_address, required=True,
+                        help='指定 DNS 服务器地址。必须。')
+    parser.add_argument('-t', '--thread', type=int, default=10,
+                        help='指定线程数量，数量多，就快。但是不要超过10倍的cpu核心数，否则可能起到反效果')
+
+    # -a 参数当 -s 或 -e 参数出现时必须提供
+    parser.add_argument('-a', '--address-list', type=argparse.FileType('r'),
+                        help='指定源地址的地址列表文件路径。',
+                        required='-s' in sys.argv or '--spoof-source' in sys.argv or '-e' in sys.argv or '--ecs-enable' in sys.argv)
+    parser.add_argument('--debug', action='store_true', dest='debug',
+                        help='Enable debug 打印所有日志')
+    parser.add_argument('--timeout', type=int, default=3,
+                        help='超时时间')
+
+    parser.add_argument('domain_list', nargs='?', type=argparse.FileType('r'),
+                        help='指定域名列表的文件路径。')
+
+    return parser.parse_args()
+
+
+def validate_ip_address(ip):
+    try:
+        socket.inet_pton(socket.AF_INET, ip)  # 尝试解析 IPv4 地址
+        return ip
+    except socket.error:
+        try:
+            socket.inet_pton(socket.AF_INET6, ip)  # 尝试解析 IPv6 地址
+            return ip
+        except socket.error:
+            raise argparse.ArgumentTypeError(f"Invalid IP address: {ip}")
+
+
+def expand_addresses(file_io: io.TextIOBase):
+    addresses = []
+
+    # 读取文件内容
+    lines = file_io.readlines()
+
+    # 处理每行内容
+    for line in lines:
+        # 去除注释和两端空格
+        line = line.split('#')[0].strip()
+
+        # 检查是否为空行
+        if not line:
+            continue
+
+        # 解析 IP、IP 范围和网络掩码
+        try:
+            # 如果是 IP 地址
+            if '/' not in line and '-' not in line:
+                addresses.append(str(ipaddress.IPv4Address(line)))
+            # 如果是 IP 范围
+            elif '-' in line:
+                start_ip, end_ip = line.split('-')
+                start_ip = ipaddress.IPv4Address(start_ip.strip())
+                end_ip = ipaddress.IPv4Address(end_ip.strip())
+                for ip in range(int(start_ip), int(end_ip) + 1):
+                    addresses.append(str(ipaddress.IPv4Address(ip)))
+            # 如果是网络掩码
+            else:
+                network = ipaddress.IPv4Network(line, strict=False)
+                for ip in network:
+                    addresses.append(str(ip))
+        except ValueError:
+            print(f"Invalid address format: {line}")
+
+    return addresses
+
+
+# 从文件中读取域名列表
+def read_domain_list(file_io: io.TextIOBase):
+    domains = []
+    for line in file_io:
+        # 去除注释和两端空格
+        line = line.split('#')[0].strip()
+        if line:
+            domain, record_type = line.split()
+            domains.append((domain, record_type))
+    return domains
+
+
+# 处理单个域名的 DNS 查询
+def process_domain(logger, dns_handler, dnsserver, domain_list, source_ip, output_dir):
+    sub_response = []
+    for domain, record_type in domain_list:
+        response = dns_handler.dns_query(dnsserver, domain, record_type, client_ip=source_ip, src=source_ip)
+        logger.debug(f"DNS query for {domain} from {source_ip}: {response}")
+        sub_response.append(response)
+
+    # 将查询结果写入 CSV 文件
+    csv_file = os.path.join(output_dir, f"{source_ip}.csv")
+    with open(csv_file, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(sub_response)  # response 需要替换为实际的查询结果变量名
+
+    # global completed_threads
+    # completed_threads += 1
+    # # update_progress()
+
+
+# 主函数
+if __name__ == "__main__":
+    # 调用函数解析命令行参数
+    args = parse_arguments()
+
+    logger = setup_logger('run.log')
+    if args.debug:
+        logger.setLevel('DEBUG')
+
+    # 初始化 DNSHandler 和 logger
+    dns_handler = DNSHandler()
+    dns_handler.query_timeout = args.timeout
+
+    # 读取域名列表
+    domain_list = read_domain_list(args.domain_list)
+
+    # 生成不同源 IP 地址列表
+    source_ips = expand_addresses(args.address_list)  # 举例
+
+    dns_servername = args.dnsserver
+    # 创建线程池
+    with ThreadPoolExecutor(max_workers=args.thread) as executor:
+        # 初始化进度条
+        total_tasks = len(source_ips)
+        progress_bar = tqdm(total=total_tasks, desc="Threads", position=0, mininterval=0.1)
+
+        # 遍历源 IP 地址列表
+        for source_ip in source_ips:
+            # 创建源 IP 目录
+            output_dir = os.path.join('output', dns_servername)
+            os.makedirs(output_dir, exist_ok=True)
+            # 将源IP和域名列表提交到线程池
+            future = executor.submit(process_domain, logger, dns_handler, dns_servername, domain_list, source_ip,
+                                     output_dir)
+            # 在任务完成时更新进度条
+            future.add_done_callback(lambda x: progress_bar.update())
+
+        # 等待所有任务完成
+        executor.shutdown()
+
+        # 结束进度条
+        progress_bar.close()
+
+    # 打印空行，避免进度打印覆盖
+    print()
---
+++ Resolve.py
@@ -0,0 +1,162 @@
+import dns.edns
+import dns.message
+import dns.rdatatype
+from dns import message as dns_message
+from scapy.all import *
+from scapy.layers.dns import DNS
+from scapy.layers.inet import IP, UDP
+
+
+def setup_logger(log_file):
+    # 检查是否已经存在具有相同名称的 logger 对象
+    if log_file in logging.Logger.manager.loggerDict:
+        return logging.getLogger("runlog")
+
+    # 创建一个logger
+    logger = logging.getLogger("runlog")
+
+    # 创建一个handler，用于写入日志文件
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(logging.DEBUG)
+
+    # 创建一个handler，用于输出到控制台
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+
+    # 定义handler的输出格式
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+
+    # 给logger添加handler
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    return logger
+
+
+class DNSHandler:
+    flag_RD = False
+    loger = setup_logger("DNSHandler.log")
+    query_timeout = 2
+
+    def create_dns_request(self, domain, record_type, client_ip=None):
+        try:
+            # 创建 DNS 请求消息
+            request = dns_message.make_query(domain, dns.rdatatype.__dict__[record_type])
+            if not self.flag_RD:
+                request.flags &= ~dns.flags.RD
+
+            # 添加 EDNS 选项（如果提供了客户端 IP）
+            if client_ip:
+                ecs_option = dns.edns.ECSOption(client_ip, srclen=32)
+                request.use_edns(edns=True, options=[ecs_option], payload=4096)
+            else:
+                # 没有提供客户端 IP，则不携带 EDNS 选项，考虑了一下不携带也开启edns吧，防止返回过大导致截断。
+                request.use_edns(edns=True, payload=4096)
+
+            # 将 DNS 请求消息转换为字节数据
+            dns_req_bytes = request.to_wire()
+
+            return dns_req_bytes
+        except (dns.message.UnknownRdatatype, dns.name.LabelTooLong) as e:
+            self.loger.error("Error creating DNS request:", e)
+            return None
+
+    def send_dns_request(self, dst_ip, dns_req_bytes, src=None):
+        try:
+            # 构造 DNS 数据包
+            src_port = RandShort()  # 构造随机端口，不然源端口全是53
+            if src:
+                dns_packet = IP(src=src, dst=dst_ip) / UDP(dport=53, sport=src_port) / DNS(dns_req_bytes)
+                # 发送 DNS 数据包
+                dns_response = sr1(dns_packet, verbose=0, timeout=self.query_timeout)
+            else:
+                dns_packet = IP(dst=dst_ip) / UDP(dport=53, sport=src_port) / DNS(dns_req_bytes)
+                # 发送 DNS 数据包
+                dns_response = sr1(dns_packet, verbose=0, timeout=self.query_timeout)
+
+            return dns_response
+        except Exception as e:
+            self.loger.error("Error sending DNS request:", e)
+            return None
+
+    def extract_ips(self, answer_records):
+        # print(answer_records)
+        # 从 answer 记录中提取 IP 地址
+        extracted_ips = []
+        if '\n' in answer_records:
+            for rdata_sp in answer_records.split('\n'):
+                extracted_ips.append(' '.join(rdata_sp.split()[4:]))
+        else:
+            extracted_ips.append(' '.join(answer_records.split()[4:]))
+        return ';'.join(sorted(extracted_ips))
+
+    def dns_query(self, dnsserver, domain, record_type, client_ip=None, src=None):
+        dns_req_bytes = self.create_dns_request(domain, record_type, client_ip)
+        if dns_req_bytes is not None:
+            dns_response = self.send_dns_request(dnsserver, dns_req_bytes, src=src)
+            if dns_response is not None:
+                dns_resp_bytes = bytes(dns_response[DNS])
+                dns_resp_msg = dns_message.from_wire(dns_resp_bytes)
+                rcode = dns_resp_msg.rcode().value
+                # 当 rcode 为 NXDOMAIN 时，不需要返回任何内容
+                if rcode == 3:
+                    return "NXDOMAIN", domain, record_type, ""
+                elif rcode == 0:
+                    # 如果 answer 记录不为空，获取 A 记录的 IP 地址
+                    answer_records = dns_resp_msg.answer
+                    authority_records = dns_resp_msg.authority
+                    additional_records = dns_resp_msg.additional
+                    if answer_records:
+                        extracted_ips = []
+                        for i in answer_records:
+                            extracted_ips.append(self.extract_ips(i.to_text()))
+                        return "NOERROR", domain, record_type, ';'.join(extracted_ips)
+                    elif authority_records:
+                        extracted_ips_au = []
+                        for i in authority_records:
+                            if "\n" in i.to_text():
+                                extracted_ips_au.extend(i.to_text().split("\n"))
+                            else:
+                                extracted_ips_au.append(i.to_text())
+                        if additional_records:
+                            for additional in additional_records:
+                                if "\n" in additional.to_text():
+                                    extracted_ips_au.extend(additional.to_text().split("\n"))
+                                else:
+                                    extracted_ips_au.append(additional.to_text())
+                        # print(';'.join(extracted_ips_au))
+                        return "NoAnswer", domain, record_type, ';'.join(extracted_ips_au)
+                else:
+                    return rcode_to_string(rcode), domain, record_type, ""
+            else:
+                self.loger.error(f"{dnsserver} No response received. :{domain} src:{src}")
+                return 'TIMEOUT', domain, record_type, ""
+        else:
+            return 'TIMEOUT', domain, record_type, ""
+
+
+def rcode_to_string(rcode):
+    rcode_dict = {
+        0: "NOERROR",
+        1: "FORMERR",
+        2: "SERVFAIL",
+        3: "NXDOMAIN",
+        4: "NOTIMP",
+        5: "REFUSED",
+        # 添加其他可能的返回码...
+    }
+
+    return rcode_dict.get(rcode, "UNKNOWN")
+
+
+# 示例用法
+if __name__ == "__main__":
+    dns_handler = DNSHandler()
+    loger = setup_logger('run.log')
+    loger.info(dns_handler.dns_query('192.168.123.1', 'www.baidu.com', 'A', '192.168.123.22'))
+    loger.info(dns_handler.dns_query('192.168.123.1', 'www.baidu.com', 'A'))
+    loger.info(dns_handler.dns_query('192.168.123.1', 'www.baidu.com', 'A', src='1.1.1.1'))
+    loger.info(dns_handler.dns_query('192.168.123.1', 'www.baidu.com', 'A', client_ip='8.8.8.8', src='1.1.1.1'))
+    loger.info(dns_handler.dns_query('114.114.114.114', 'www.chihuo.fun', 'A'))
